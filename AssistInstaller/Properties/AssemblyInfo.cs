07e7b24556264eaae212e6e06a247d6f6b905b3b
Michael <49486947+HeyM1ke@users.noreply.github.com>
2022-05-07 09:54:45 -0400

    V1.0.0.2

--- AssistInstaller/Properties/AssemblyInfo.cs
+++ AssistInstaller/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AssistInstaller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Installer for Assist Program")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AssistTeam")]
 [assembly: AssemblyProduct("AssistInstaller")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
